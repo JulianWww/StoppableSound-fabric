58594ca9deae9d870d717c18a635b1b96be4e4f4
Julian Wandhoven <Julian.wandhoven@gmail.com>
2023-01-09 16:26:36 +0100

    added time builder

---
+++ src/main/java/net/denanu/stoppablesound/utils/TimeBuilder.java
@@ -0,0 +1,33 @@
+package net.denanu.stoppablesound.utils;
+
+public class TimeBuilder {
+	int h = 0, m = 0, s = 0, t = 0;
+
+	public TimeBuilder of() {
+		return new TimeBuilder();
+	}
+
+	public TimeBuilder tick(final int tick) {
+		this.t = tick;
+		return this;
+	}
+
+	public TimeBuilder sec(final int sec) {
+		this.s = sec;
+		return this;
+	}
+
+	public TimeBuilder min(final int min) {
+		this.m = min;
+		return this;
+	}
+
+	public TimeBuilder hour(final int hour) {
+		this.h = hour;
+		return this;
+	}
+
+	public int build() {
+		return ((60*this.h + this.m) * 60 + this.s) * 20 + this.t;
+	}
+}
